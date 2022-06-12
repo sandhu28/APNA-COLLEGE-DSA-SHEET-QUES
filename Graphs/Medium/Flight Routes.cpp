de30273ecf21608de697b3993d660401e26c8098
sandhu28 <sandhu.1@iitj.ac.in>
2022-06-12 19:43:21 +0530

    next commit

---
+++ Graphs/Medium/Flight Routes.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#define pii pair<int,int>
+#define lli long long int
+#define li long int
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vpii vector<pair<int,int>>
+#define vlli vector<lli>
+#define vli vector<li>
+#define rep(i,k,n) for (int i = k; i < n; ++i)
+#define Repr(i,k,n) for (int i = k; i >= n; --i)
+#define c(n) cout<<n<<endl
+#define z(n) cout<<n<<" " 
+#define nl cout<<endl;
+#define INF INT_MAX
+using namespace std;
+
+
+
+int main(){
+    
+    int n,m,k;
+    cin>>n>>m>>k;
+
+    return 0;
+}
