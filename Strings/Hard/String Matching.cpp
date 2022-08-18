c01259ab5978f53bc749f2fa5c2c84afe39bf1f7
Sukhman Sandhu <sandhu.1@iitj.ac.in>
2022-08-18 23:17:38 +0530

    next commit

---
+++ Strings/Hard/String Matching.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#define pii pair<int,int>
+#define int long long int
+#define li long int
+#define vi vector<int>
+#define vd vector<double>
+#define vvd vector<vector<double>>
+#define vvi vector<vector<int>>
+#define vpii vector<pair<int,int>>
+#define vlli vector<lli>
+#define vli vector<li>
+#define vs vector<string>
+#define ss second
+#define ff first
+#define all(x) (x).begin(),(x).end()
+#define fo(i,n) for(int i=0;i<n;i++)
+#define foe(i,k,n) for (int i = k; i < n; ++i)
+#define Repr(i,k,n) for (int i = k; i >= n; --i)
+#define c(n) cout<<n<<endl
+#define z(n) cout<<n<<" " 
+#define nl cout<<endl;
+#define INF INT_MAX
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL)
+using namespace std;
+
+const int M = 1e9 + 7;
+const int N = 1e5+ 3;
+
+
+
+int32_t main(){
+    fast_io;
+    string s,p;
+    cin>>s>>p;
+    
+    
+    return 0;
+}
